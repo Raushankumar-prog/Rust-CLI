107bd1ee5e8a4f4d1d48c89084c198272133e66b
Raushan kumar <raushankumar8757825@gmail.com>
2025-06-24 10:32:04 +0530

    starting making of rust-cli

---
+++ target/debug/deps/rust_cli-238681f4de54977e.d
@@ -0,0 +1,5 @@
+C:\Users\ACER\desktop\github\rust\rust-cli\target\debug\deps\librust_cli-238681f4de54977e.rmeta: src\main.rs
+
+C:\Users\ACER\desktop\github\rust\rust-cli\target\debug\deps\rust_cli-238681f4de54977e.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/rust_cli-86103717388c7012.d
@@ -0,0 +1,5 @@
+C:\Users\ACER\desktop\github\rust\rust-cli\target\debug\deps\librust_cli-86103717388c7012.rmeta: src\main.rs
+
+C:\Users\ACER\desktop\github\rust\rust-cli\target\debug\deps\rust_cli-86103717388c7012.d: src\main.rs
+
+src\main.rs:
