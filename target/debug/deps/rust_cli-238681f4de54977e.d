0aee064246b1bb638aab87b0eb0ad6f7a615d94c
Raushan kumar <raushankumar8757825@gmail.com>
2025-06-27 06:41:23 +0530

    removing target

--- target/debug/deps/rust_cli-238681f4de54977e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ACER\desktop\github\rust\rust-cli\target\debug\deps\librust_cli-238681f4de54977e.rmeta: src\main.rs
-
-C:\Users\ACER\desktop\github\rust\rust-cli\target\debug\deps\rust_cli-238681f4de54977e.d: src\main.rs
-
-src\main.rs:
